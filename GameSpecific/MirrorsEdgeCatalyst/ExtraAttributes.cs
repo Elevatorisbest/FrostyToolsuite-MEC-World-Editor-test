ee5a0159384d7d759e66989a33d52451711f551d
Komander-3 <mrhwam@gmail.com>
2025-01-11 17:07:43 +0100

    Ported WidgetEditor functionality from MEA LvlEd Removed... desktop.ini files??? how did they get here?!

---
+++ GameSpecific/MirrorsEdgeCatalyst/ExtraAttributes.cs
@@ -0,0 +1,23 @@
+﻿using LevelEditorPlugin.Definitions;
+using LevelEditorPlugin.Extensions;
+using Frosty.Core.Attributes;
+using FrostySdk;
+
+// Extra attributes are placed here that are specific to this game
+
+[assembly: PluginValidForProfile((int)ProfileVersion.MirrorsEdgeCatalyst)]
+
+// logic blueprints
+
+// object blueprints
+[assembly: RegisterAssetDefinition("UIWidgetBlueprint", typeof(UIWidgetBlueprintAssetDefinition))]
+[assembly: RegisterAssetDefinition("PamCharacterBlueprint", typeof(PamCharacterBlueprintAssetDefinition))]
+[assembly: RegisterAssetDefinition("AppearanceEntityBlueprint", typeof(AppearanceEntityBlueprintAssetDefinition))]
+[assembly: RegisterAssetDefinition("GameObjectBlueprint", typeof(GameObjectBlueprintAssetDefinition))]
+[assembly: RegisterAssetDefinition("CharacterBlueprint", typeof(CharacterBlueprintAssetDefinition))]
+[assembly: RegisterAssetDefinition("EffectBlueprint", typeof(EffectBlueprintAssetDefinition))]
+[assembly: RegisterAssetDefinition("VehicleBlueprint", typeof(VehicleBlueprintAssetDefinition))]
+[assembly: RegisterAssetDefinition("VisualEnvironmentBlueprint", typeof(VisualEnvironmentBlueprintAssetDefinition))]
+
+// other
+[assembly: RegisterAssetDefinition("SubWorldData", typeof(SubWorldDataAssetDefinition))]
--- GameSpecific/MirrorsEdgeCatalyst/GameShared/UITextureMappingAsset.cs
+++ GameSpecific/MirrorsEdgeCatalyst/GameShared/UITextureMappingAsset.cs
@@ -1,4 +1,6 @@
 ﻿using Frosty.Core;
+using FrostySdk;
+using FrostySdk.Ebx;
 using FrostySdk.Resources;
 using System;
 using System.Collections.Generic;
@@ -13,20 +15,37 @@ namespace LevelEditorPlugin.Assets
     {
         public FrostySdk.Ebx.UITextureMappingAsset Data => data as FrostySdk.Ebx.UITextureMappingAsset;
 
-        protected Dictionary<ulong, BitmapImage> textureAssets = new Dictionary<ulong, BitmapImage>();
-        protected Dictionary<int, ulong> textureIds = new Dictionary<int, ulong>();
+        protected Dictionary<PointerRef, BitmapImage> textureAssets = new Dictionary<PointerRef, BitmapImage>();
+        protected Dictionary<CString, PointerRef> textureIds = new Dictionary<CString, PointerRef>();
 
         public UITextureMappingAsset(Guid fileGuid, FrostySdk.Ebx.UITextureMappingAsset inData)
             : base(fileGuid, inData)
         {
+            //foreach (var output in Data.Output)
+            //{
+            //    if (!textureAssets.ContainsKey(output.Texture))
+            //    {
+            //        var texture = App.AssetManager.GetResAs<Texture>(output.Texture.External.FileGuid());
+            //        var bitmap = TextureCreator.CreatePNG(texture);
+
+            //        textureAssets.Add(output.Texture, bitmap);
+            //    }
+            //    textureIds.Add(output.Id, output.Texture);
+            //}
         }
 
-        public BitmapImage GetTexture(int id)
-        {
-            if (!textureIds.ContainsKey(id))
-                return null;
+        //public BitmapImage GetTexture(CString id)
+        //{
+        //    if (!textureIds.ContainsKey(id))
+        //        return null;
 
-            return textureAssets[textureIds[id]];
-        }
+        //    return textureAssets[textureIds[id]];
+        //}
+
+        //public Point4D GetUVRect(CString id)
+        //{
+        //    var result = Data.Output.FirstOrDefault(t => t.Id == id);
+        //    return new Point4D(result.Min.x, result.Min.y, result.Max.x, result.Max.y);
+        //}
     }
 }
